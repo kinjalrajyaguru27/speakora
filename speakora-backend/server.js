161bacfd9a8b5f16451dabc52f38d6f6b796b2d5
kinjalrajyaguru27 <rajyagurukinjal27@gmail.com>
2026-06-18 22:11:58 +0530

    Fix Render PORT

--- speakora-backend/server.js
+++ speakora-backend/server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../home.html"));
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
 });
