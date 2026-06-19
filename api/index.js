7f723de0d7ca5e5d8efd09f4e66c73e638f88e8f
kinjalrajyaguru27 <rajyagurukinjal27@gmail.com>
2026-06-19 20:15:07 +0530

    Configure Vercel serverless function entrypoint and dependency installation at root

---
+++ api/index.js
@@ -0,0 +1,2 @@
+const app = require("../speakora-backend/server.js");
+module.exports = app;
--- speakora-backend/server.js
+++ speakora-backend/server.js
@@ -39,9 +39,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
-});
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log("Server started on port " + PORT);
+  });
+}
 
 module.exports = app;
